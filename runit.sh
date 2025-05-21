1dce9f7fa78af57982afd6210032a2c4e4c3316a
brianbrix <mokandubrian@gmail.com>
2025-05-21 14:03:01 +0300

    Import

--- runit.sh
+++ runit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-CONTAINER_NAME=$1
+DB_HOST_CONTAINER_NAME=$1
 NETWORK_NAME=backend
 
-if [ -z "$CONTAINER_NAME" ]; then
+if [ -z "$DB_HOST_CONTAINER_NAME" ]; then
   echo "Usage: $0 <container-name>"
   exit 1
 fi
 
 # Check if the container is already in the network
-if docker inspect "$CONTAINER_NAME" | grep -q "\"$NETWORK_NAME\":"; then
-  echo "Container '$CONTAINER_NAME' is already connected to the '$NETWORK_NAME' network."
+if docker inspect "$DB_HOST_CONTAINER_NAME" | grep -q "\"$NETWORK_NAME\":"; then
+  echo "Container '$DB_HOST_CONTAINER_NAME' is already connected to the '$NETWORK_NAME' network."
 else
-  echo "Connecting '$CONTAINER_NAME' to '$NETWORK_NAME'..."
-  docker network connect "$NETWORK_NAME" "$CONTAINER_NAME"
+  echo "Connecting '$DB_HOST_CONTAINER_NAME' to '$NETWORK_NAME'..."
+  docker network connect "$NETWORK_NAME" "$DB_HOST_CONTAINER_NAME"
   echo "Done."
 fi
 
