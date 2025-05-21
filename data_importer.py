d9bd4a5f6df4fffa857a46f27bf74bc6caa66a7a
brianbrix <mokandubrian@gmail.com>
2025-05-21 13:14:23 +0300

    Import

--- data_importer.py
+++ data_importer.py
@@ -107,9 +107,9 @@ def get_data(excel_file: str, skip_rows: int, sheet_name: str):
     # print("categories", categories)
 
     all_orgs = get_organizations(agencies)
-    run_sql_file_postgres('insert_orgs.sql')
-    add_sectors_to_db(sectors)
-    insert_categories(file_categories)
+    # run_sql_file_postgres('insert_orgs.sql')
+    # add_sectors_to_db(sectors)
+    # insert_categories(file_categories)
     categories = get_category_values(list(mapping_dict.keys()))
     all_currencies = get_currencies()
     all_adj_types = get_adjustment_types()
