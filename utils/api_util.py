82157171a3e6ab7c634920201bd411ed6707cb47
brianbrix <mokandubrian@gmail.com>
2025-06-03 13:10:15 +0300

    Add update

--- utils/api_util.py
+++ utils/api_util.py
@@ -86,7 +86,7 @@ def import_project(json_data, id: Number):
     logging.info(json_data)
     post_url = conf.get('api', 'baseurl')+'/rest/activity?can-downgrade-to-draft=true'
     if id is not None:
-        logging.info("Updating project with id: %s and title", id, json_data['project_title'])
+        logging.info("Updating project with id: %s and title %s", id, json_data['project_title'])
         post_url = conf.get('api', 'baseurl')+'/rest/activity/'+str(id)+'?can-downgrade-to-draft=true'
     response = post_with_cookie(post_url, json_data)
     logging.info(response)
