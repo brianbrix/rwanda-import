5a9ae658c72c8e4f69b78197056fb8d87958c33d
brianbrix <mokandubrian@gmail.com>
2025-06-03 08:52:13 +0300

    Add airflow

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -9,11 +9,10 @@ def get_db_connection():
     global _connection
     if _connection is None or _connection.closed != 0:
         _connection = psycopg2.connect(
-            dbname=os.getenv("POSTGRES_DB", os.getenv('AMP_DB_NAME')),
-            user=os.getenv("POSTGRES_USER", os.getenv('AMP_DB_USER')),
-            password=os.getenv("POSTGRES_PASSWORD", os.getenv('AMP_DB_PASSWORD')),
-            host=os.getenv("POSTGRES_HOST", os.getenv('AMP_DB_HOST')),  # since you're on the host
-            port=os.getenv("POSTGRES_PORT", os.getenv('AMP_DB_PORT')),
-            cursor_factory=RealDictCursor
+            dbname=os.getenv('AMP_DB_NAME'),
+            user=os.getenv('AMP_DB_USER'),
+            password=os.getenv('AMP_DB_PASSWORD'),
+            host=os.getenv('AMP_DB_HOST'),  # since you're on the host
+            port=os.getenv('AMP_DB_PORT')
         )
     return _connection
